878b0c03ed39798505f7f592c17dc167e34019ec
EduardoGMora <lalogomo2000@outlook.com>
2024-10-14 01:40:50 -0600

    inicialización del programa 3 ahora sin los lotes y con 5 estados de cada proceso (faltan funciones para actualizar y mandar los procesos a estos estados)

--- Programa 3/README.md
+++ Programa 3/README.md
@@ -1,1 +1,10 @@
-procesamiento por lotes tomando encuenta los 5 estados de procesamiento
+# Practica 3: Algoritmo de planificación FCFS
+
+Procesamiento por lotes tomando encuenta los 5 estados de procesamiento
+
+1. Nuevo: Procesos que se acaban de crear, pero aún no han sido admitidos por el
+sistema operativo en el grupo de procesos ejecutables.
+2. Listos: Procesos que están preparados para ejecutarse, en cuanto sea su turno.
+3. Ejecución: Proceso que está actualmente en ejecución.
+4. Bloqueado: Proceso que no puede ejecutar hasta que se produzca cierto suceso, como la terminación de una operación de E/S.
+5. Terminado: Un proceso que ha sido excluido por el sistema operativo del grupo de procesos activos, bien porque se detuvo o porque fue abandonado por alguna razón.
---
+++ Programa 3/practica3.py
@@ -0,0 +1,319 @@
+import random
+import tkinter as tk
+
+class Procesos:  #clase de procesos o nodos
+  #atributos
+  def __init__(self, Id, operacion, tme):  
+    self.Id = Id
+    self.operacion = operacion
+    self.tme = tme
+    self.ejecutado = False
+    self.next = None  # Inicializar el apuntador al siguiente nodo como None
+
+  @staticmethod
+  def getOperacion():
+    operadores = ['+', '-', '*', '/', '%']  # Lista de operadores matemáticos
+    num1 = random.randint(0, 100)
+    num2 = random.randint(0, 100)
+
+    operador = random.choice(operadores)
+
+    if num2 == 0 and operador == '/' or operador == '%':
+      num2 = random.randint(1, 9)
+
+    operacion = f"{num1} {operador} {num2}"
+
+    return operacion
+              
+  @staticmethod
+  def getTME():
+    tme = random.randint(5, 18) # Tiempo de ejecución aleatorio entre 5 y 18 segundos
+    return tme
+
+class LL:  #clase de estructura de datos Linked list
+  def __init__(self):  #apuntadores
+    self.head = None
+    self.tail = None
+
+  #métodos
+  def agregarTail(self, Id, operacion, tme):  #agregar al final
+    nuevoProceso = Procesos(Id, operacion, tme)
+    if self.tail is None:
+      self.head = nuevoProceso
+      self.tail = nuevoProceso
+    else:
+      self.tail.next = nuevoProceso
+      self.tail = nuevoProceso
+
+  # def switch(self, tamano_lote):      # switch del proceso en espera y el último proceso del lote
+  #   if self.head is None or self.head.next is None:
+  #     return
+    
+  #   temp = self.head
+  #   self.head = self.head.next
+    
+  #   self.insertar(temp.Id, temp.operacion, temp.tme, tamano_lote)
+
+  # def insertar(self, Id, operacion, tme, tamano_lote):  #insertar un proceso en una posición específica
+  #   nuevoProceso = Procesos(Id, operacion, tme)
+    
+  #   # Caso de lista vacía, insertar al principio
+  #   if self.head is None:
+  #       self.head = nuevoProceso
+  #       return
+    
+  #   temp = self.head
+  #   j = 0
+
+  #   while temp.next is not None and j < tamano_lote - 1:
+  #     temp = temp.next
+  #     j += 1
+    
+  #   # Insertar el nuevo proceso al final del lote
+  #   nuevoProceso.next = temp.next
+  #   temp.next = nuevoProceso
+
+  def borrarHead(self):  #borrar el primero
+    if self.head is None:
+      return None
+    temp = self.head
+    self.head = temp.next
+    if self.head is None:
+      self.tail = None
+    return temp
+
+  def peekFront(self):  #ver el primero
+    return self.head
+
+  def contar(self):  #contar los números de procesos
+    temp = self.head
+    i = 0
+    while temp is not None:
+      temp = temp.next
+      i += 1
+    return i
+  
+  def mostrarLista(self):  #mostrar la lista de procesos
+    temp = self.head
+    while temp is not None:
+      print(f'\nNúmero de proceso: {temp.Id}')
+      print(f"Resultado: {temp.operacion}")
+      print(f"Tiempo de ejecución: {temp.tme}")
+      temp = temp.next
+
+  def mostrarProceso(self, proceso):  #mostrar un proceso
+    print(f'Número de proceso: {proceso.Id}')
+    print(f"Resultado: {proceso.operacion}")
+    print(f"Tiempo de ejecución: {proceso.tme}")
+
+  def buscar(self,Id):
+    temp = self.head
+    while temp is not None:
+      if temp.Id == Id:
+        return self.mostrarProceso(temp)
+      temp = temp.next
+    return None
+
+class Ventana:
+  def __init__(self, ventana, listaEspera, listaTerminados):
+    self.ventana = ventana          #atributos
+    self.ventana.title("Procesamiento por lotes (First Come First Server)")
+
+    # instancia de la clase LL
+    self.listaEspera = listaEspera
+    self.listaTerminados = listaTerminados
+    self.procesoactual = None       #inicializa el apuntador al proceso en ejecución
+    
+    # atributos de ayuda
+    self.contador = 0
+    self.tiempo = 0
+    self.pausado = False
+    self.relojglobal = tk.Label(ventana, text=f"Reloj Global: {self.tiempo}")
+    self.relojglobal.grid(row=0, column=4, padx=150)
+
+    #label
+    etiqueta = tk.Label(ventana, text=f'Número de procesos: {listaEspera.contar()}', font="arial 12")
+    etiqueta.grid(row=0, column=0, pady=10)
+    estado1 = tk.Label(ventana, text="NUEVO", font="arial 12")
+    estado1.grid(row=1, column=0, pady=10)
+    estado2 = tk.Label(ventana, text="LISTOS", font="arial 12")
+    estado2.grid(row=1, column=2, pady=10)
+    estado3 = tk.Label(ventana, text="EJECUCIÓN", font="arial 12")
+    estado3.grid(row=1, column=4, pady=10)
+    estado4 = tk.Label(ventana, text="BLOQUEADO", font="arial 12")
+    estado4.grid(row=1, column=6, pady=10)
+    estado5 = tk.Label(ventana, text="TERMINADO", font="arial 12")
+    estado5.grid(row=1, column=8, pady=10)
+
+    #procesos en nuevo
+    self.nuevo = tk.Text(ventana, width=30, borderwidth=4, bg="sky blue", state=tk.DISABLED)
+    self.nuevo.grid(row=2, column=0, padx=30)
+    #procesos en listos
+    self.listos = tk.Text(ventana, width=30, borderwidth=4, bg="pale green", state=tk.DISABLED)
+    self.listos.grid(row=2, column=2, padx=30)
+    #procesos ejecucion
+    self.ejecucion = tk.Text(ventana, width=30, borderwidth=4, bg="light goldenrod", state=tk.DISABLED)
+    self.ejecucion.grid(row=2, column=4, padx=30)
+    #procesos bloqueados
+    self.bloqueados = tk.Text(ventana, width=30, borderwidth=4, bg="tomato", state=tk.DISABLED)
+    self.bloqueados.grid(row=2, column=6, padx=30)
+    #procesos terminados
+    self.terminado = tk.Text(ventana, width=30, borderwidth=4, bg="pale violet red", state=tk.DISABLED)
+    self.terminado.grid(row=2, column=8, padx=30)
+
+    #botón de inicio
+    self.ventana.bind("<space>", lambda event: self.iniciar())  # Espacio para iniciar
+    self.boton = tk.Button(ventana, text="Iniciar", command=self.iniciar)
+    self.boton.grid(row=3, column=4, pady=10)
+    self.ventana.bind("<Return>", lambda event: self.ventana.quit()) # Enter para salir
+
+
+  def actualizarEspera(self):  #actualiza la interfaz de espera
+    texto = ""
+    temp = self.listaEspera.head
+    while temp is not None:
+      texto += f'{temp.Id}.- {temp.operacion}\n TME: {temp.tme}\n\n'
+      temp = temp.next
+    
+    if texto == "":
+      texto = "No hay más procesos en espera."  # Si no hay más lotes
+
+    self.espera.config(state=tk.NORMAL)
+    self.espera.delete('1.0', tk.END)
+    self.espera.insert(tk.END, texto)
+    self.espera.config(state=tk.DISABLED)
+
+  def actualizarEjecucion(self): #actualiza la interfaz de ejecución
+    if not self.pausado:
+      self.actualizarReloj()
+
+      if self.procesoactual is not None:
+        self.procesoactual.tme -= 1  
+        texto = f'{self.procesoactual.Id}.- {self.procesoactual.operacion}\n TME: {self.procesoactual.tme} \nTiempo de ejecución: {self.contador} segundos\n\n'
+        
+        self.ejecucion.config(state=tk.NORMAL)  
+        self.ejecucion.delete('1.0', tk.END)  
+        self.ejecucion.insert(tk.END, texto)  
+        self.ejecucion.config(state=tk.DISABLED)  
+
+        self.contador += 1
+                
+        # Si el TME llega a 0, pasar al siguiente proceso
+        if self.procesoactual.tme == 0:
+            resultado = eval(self.procesoactual.operacion)
+            self.listaTerminados.agregarTail(self.procesoactual.Id, resultado, self.contador)
+            self.actualizarTerminados()
+            self.listaEspera.borrarHead()
+            self.contador = 0
+            self.procesoactual.ejecutado = True
+            self.procesoactual = self.procesoactual.next
+
+      else:
+        texto = "\nTodos los procesos han terminado."
+        self.ejecucion.config(state=tk.NORMAL)
+        self.ejecucion.delete('1.0', tk.END)
+        self.ejecucion.insert(tk.END, texto)
+        self.ejecucion.config(state=tk.DISABLED)
+        return
+
+    self.ventana.after(1000, self.actualizarEjecucion)  # Se ejecutará de nuevo en 1 segundo
+
+
+  def actualizarTerminados(self):  #actualiza la interfaz de terminados
+    texto = ""
+    temp = self.listaTerminados.head
+
+    while temp is not None:
+      texto += f'{temp.Id}.- Resultado de la operación: {temp.operacion}\n\n\n'
+      temp = temp.next
+    
+    self.actualizarEspera()
+    
+    self.terminado.config(state=tk.NORMAL)  
+    self.terminado.delete('1.0', tk.END)  
+    self.terminado.insert(tk.END, texto)  
+    self.terminado.config(state=tk.DISABLED)  
+  
+  def iniciar(self):
+    self.procesoactual = self.listaEspera.peekFront()
+    self.boton.config(state=tk.DISABLED)
+
+    # Entradas del teclado
+    self.ventana.bind("<i>", lambda evebt: self.interrupcion())
+    self.ventana.bind("<e>", lambda event: self.error())
+    self.ventana.bind("<p>", lambda event: self.pausa())
+    self.ventana.bind("<c>", lambda event: self.continuar())
+
+    self.actualizarEspera()
+    self.actualizarEjecucion()
+
+  def interrupcion(self):
+    # Mueve el proceso actual al final del lote
+    if self.procesoactual.next is not None:
+      self.procesoactual = self.listaEspera.peekFront()
+      self.actualizarEspera()
+      self.actualizarEjecucion()
+    else:
+      return
+
+  def error(self):
+    if self.procesoactual is not None:
+      # Agregar el proceso actual a la lista de terminados con estado de ERROR
+      self.listaTerminados.agregarTail(self.procesoactual.Id, "ERROR", self.contador)
+      self.contador = 0
+      self.actualizarTerminados()
+      self.procesoactual.ejecutado = True
+      self.procesoactual = self.procesoactual.next
+      self.listaEspera.borrarHead()
+      self.actualizarEspera()
+
+  def pausa(self):
+    while self.pausado == False:
+      # Pausa la ejecución de los procesos
+      self.pausado = True
+      print("Pausando procesos.")
+      
+    
+  def continuar(self):
+    if self.pausado:
+      # Continuar la ejecución de los procesos
+      print("Continuando procesos.")
+      self.pausado = False
+      self.actualizarEjecucion()
+
+  def actualizarReloj(self):
+    # Incrementa el tiempo y actualiza la etiqueta
+    self.tiempo += 1
+    self.relojglobal.config(text=f"Reloj Global: {self.tiempo} segundos")
+
+
+def main():
+  listaEspera = LL()  #Lista de procesos en espera
+  listaTerminados = LL()  #Lista de procesos terminados
+
+  # Crear una instancia de la clase Proceso
+  def nprocesos():
+     while True:
+       try:
+         n = int(input('\nIngrese el número de procesos -> '))
+         if n > 0:
+           return n
+         else:
+           print('\nDebe ingresar un número entero positivo')
+       except ValueError:
+         print('\nEntrada no válida. Por favor ingrese un número entero válido.')
+  
+  #hacer autocremental el Id
+  Id = 0
+  for _ in range(nprocesos()):
+    Id += 1
+    listaEspera.agregarTail(Id, Procesos.getOperacion(), Procesos.getTME())
+
+  listaEspera.mostrarLista()
+
+  ventana = tk.Tk()
+  app = Ventana(ventana, listaEspera, listaTerminados)
+  ventana.mainloop()
+
+if __name__ == "__main__":
+  main()
